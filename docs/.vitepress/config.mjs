323a65f802cb29e9fb8ee1decf7a7bc19f36520d
IT-NuanxinPro <1569250882@qq.com>
2024-07-08 15:36:02 +0800

    修改静态资源目录

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/NuanXinPro-Notes/',
   title: "NuanXinPro Notes",
   description: "Record daily front-end knowledge",
   themeConfig: {
